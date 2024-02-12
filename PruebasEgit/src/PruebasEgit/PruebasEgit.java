d058bd22020f24ca13e42924737d7b15d8076c99
Cindy <cindy.taboada@hotmail.com>
2024-02-12 10:00:00 +0100

    Otro commit rama2

--- PruebasEgit/src/PruebasEgit/PruebasEgit.java
+++ PruebasEgit/src/PruebasEgit/PruebasEgit.java
@@ -5,6 +5,7 @@ public class PruebasEgit {
 	public static void main(String[] args) {
 		System.out.println("Pruebas de Git");
 		System.out.println("Segundo Commit máster");
+		System.out.println("Rama 2");
 
 	}
 
